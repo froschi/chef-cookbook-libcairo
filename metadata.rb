1280371f071bcde18b2fb68eb2544a583bd985ef
Thorsten Fischer <thorsten@froschi.org>
2012-06-22 12:03:27 +0200

    Added dependencies for lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,14 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libdirectfb", ">= 0.0.1"
+depends          "libfontconfig", ">= 0.0.1"
+depends          "libfreetype", ">= 0.0.1"
+depends          "libpixman", ">= 0.0.1"
+depends          "libpng", ">= 0.0.1"
+depends          "libxcb-render-util", ">= 0.0.1"
+depends          "libxcb-render", ">= 0.0.1"
+depends          "libxcb-shm", ">= 0.0.1"
+depends          "libxcb", ">= 0.0.1"
+depends          "libxrender", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,24 @@
+include_recipe "libfontconfig"
+include_recipe "libfreetype"
+include_recipe "libpixman"
+include_recipe "libpng"
+include_recipe "libxcb-render"
+include_recipe "libxcb"
+include_recipe "libxrender"
+
 packages = Array.new
 
 case node[:lsb][:codename]
-when "lucid", "precise"
+when "lucid"
+  include_recipe "libdirectfb"
+  include_recipe "libxcb-render-util"
+
+  packages |= %w/
+    libcairo2
+  /
+when "precise"
+  include_recipe "libxcb-shm"
+
   packages |= %w/
     libcairo2
   /
